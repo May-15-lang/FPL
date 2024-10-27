6040d379b3177a44c8cf37ef869bd1e900490445
Mayur Vanve <mayurdv15@gmail.com>
2024-10-27 09:36:43 -0400

    Create Assignment Operator.c

---
+++ Assignment Operator.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main() {
+    int a = 10;
+    int b = 5;
+    
+    a += b; // a = a + b
+    printf("a += b: %d\n", a);
+    
+    a -= b; // a = a - b
+    printf("a -= b: %d\n", a);
+    
+    a *= b; // a = a * b
+    printf("a *= b: %d\n", a);
+    
+    a /= b; // a = a / b
+    printf("a /= b: %d\n", a);
+    
+    a %= b; // a = a % b
+    printf("a %%= b: %d\n", a);
+    
+    return 0;
+}
